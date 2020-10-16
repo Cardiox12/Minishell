88639ee1e5ea5b59ab7168fce1c3ec6ce42bdbfe
bbellavi <cardiox1212@hotmail.fr>
2020-10-16 23:47:29 +0200

    Add ifdef clause to manage 2 mains

--- main.c
+++ main.c
@@ -57,29 +57,38 @@ void	print_queue(t_queue *head)
 	ft_printf("=======================================\n");
 }
 
+#ifdef MASTER
+int		main(int argc,char *argv[], char *envp[])
+{
+	// Here is the entrypoint
+	char *line;
+
+	line = NULL;
+	while (1)
+	{
+		line = reader();
+		t_queue *tokens = lexer(line);
+		print_queue(tokens);
+		if (parser(line, tokens) != 0)
+			ft_printf("minishell: parse error\n");
+		if (line == NULL)
+			break;
+	}
+	return (0);
+}
+#endif
+
+#ifdef BUILTINS
+
 # include "builtins.h"
 # include <limits.h>
 # include <string.h>
 
 int		main(__unused int argc, __unused char *argv[], char *envp[])
 {
-	// Here is the entrypoint
-	// char *line;
-
-	// line = NULL;
-	// while (1)
-	// {
-	// 	line = reader();
-	// 	t_queue *tokens = lexer(line);
-	// 	print_queue(tokens);
-	// 	if (parser(line, tokens) != 0)
-	// 		ft_printf("minishell: parse error\n");
-	// 	if (line == NULL)
-	// 		break;
-	// }
-
 	env(NULL, envp);
 	export(argv, envp);
 	env(NULL, envp);
 	return (0);
 }
+#endif
