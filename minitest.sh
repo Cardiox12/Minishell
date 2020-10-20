c59c537c5973b0a155b124e99d436884d5287946
Macbook <theophile.lucille@gmail.com>
2020-10-21 00:55:16 +0200

    test ok, ATTENTION A BIEN IMITER BASH ET PAS ZSH

--- minitest.sh
+++ minitest.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#! /bin/bash
 
-set -x
+#set -x
 printf "Welcome to minihell's minitest\n"
 
 printf "compilation:\n"
@@ -18,22 +18,22 @@ function test_command {
 	printf "result test for :     %s\n" "$1">> test_trace
 	rm miniout1 realout1
 	minicommand=$1
-#	minicommand+=" > miniout1"
+	minicommand+=" > miniout1"
 	real_command=$1
-#	real_command+=" > realout1"
-	printf "minishell's command: %s\n" "$minicommand"
-	printf "eval's command: %s\n" "$real_command"
+	real_command+=" > realout1"
+#	printf "minishell's command: %s\n" "$minicommand"
+#	printf "eval's command: %s\n" "$real_command"
 	if printf "%s\n" "$minicommand" | ./minishell &> /dev/null
 	then
 		printf "minishell exits with success\n" | tee -a test_trace
 	else
 		printf "minishell crashes\n" | tee -a test_trace
 	fi
-	if eval "$real_command" #&> /dev/null
+	if printf "%s\n" "$real_command" | bash &> /dev/null
 	then
-		printf "eval exits correctly\n" 
+		printf "bash exits correctly\n" 
 	else
-		printf "eval crashes\n"
+		printf "bash crashes\n"
 	fi	
 	if diff miniout1 realout1 >> test_trace
 	then
@@ -49,6 +49,38 @@ function test_command {
 	printf "%s\n" '-----------------'
 }
 
+
+function test_complex_command {
+	printf "\n=====================\n"
+	printf "tested command : %s\n" "$1"
+	printf "result test: \n"
+	printf "===============\n" >> test_trace
+	printf "result test for :     %s\n" "$1">> test_trace
+	rm miniout1 realout1
+	minicommand=$1
+	minicommand+=" > miniout1"
+	printf "minishell's command: %s\n" "$minicommand"
+	printf "eval's command: %s\n" "$real_command"
+	if printf "%s\n" "$minicommand" | ./minishell &> /dev/null
+	then
+		printf "minishell exits with success\n" | tee -a test_trace
+	else
+		printf "minishell crashes\n" | tee -a test_trace
+	fi
+	echo "$2" > realout1
+	if diff miniout1 realout1 >> test_trace
+	then
+		printf "success\n" | tee -a test_trace
+	else
+		printf "failed\n" | tee -a test_trace
+		printf "\nminishell's output:\n" >> test_trace
+		cat miniout1 >> test_trace
+		printf "\neval's output:\n\n" >> test_trace
+		cat realout1 >> test_trace
+	fi
+	printf "===============\n\n" >> test_trace
+	printf "%s\n" '-----------------'
+}
 commands=("cat makefile" \
 "cat makefile | wc -l" \
 "cat makefile < main.c | grep include" \
@@ -59,8 +91,18 @@ commands=("cat makefile" \
 
 #printf "%s\n" "cat makefile < main.c | grep include | grep inc < includes/eval.h | wc" | ./minishell
 
-test_command "cat makefile < main.c | grep include | grep inc < includes/eval.h"
-eval 'cat makefile < main.c | grep include | grep inc < includes/eval.h'
-#for command in "${commands[@]}"
-#	do test_command "${command}"
-#done
+#test_command "cat makefile < main.c | grep include | grep inc < includes/eval.h"
+#eval 'cat makefile < main.c | grep include | grep inc < includes/eval.h'
+for command in "${commands[@]}"
+	do test_command "${command}"
+done
+
+test_command 'cat makefile < main.c | grep include | grep inc < includes/eval.h' 
+#set -x
+#result_command=`cat makefile < main.c | grep include | grep inc < includes/eval.h`
+#printf "result : %s\n" "$result_command"
+#test_complex_command "cat makefile < main.c | grep include | grep inc < includes/eval.h" "$result_command"
+#cat makefile < main.c | grep include | grep inc < includes/eval.h
+#cat makefile | grep include < includes/eval.h
+#echo "hello include" | grep include includes/eval.h
+#echo "hello include" | grep include #< includes/eval.h
