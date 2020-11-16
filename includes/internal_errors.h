39d1adbaf5365b645a5fbc6d02968a55181d4b50
Tony <cardiox1212@hotmail.fr>
2020-11-16 02:52:11 +0100

    Norm env, expand

--- includes/internal_errors.h
+++ includes/internal_errors.h
@@ -6,20 +6,20 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 13:12:28 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/10 14:44:35 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/16 02:49:37 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_INTERNAL_ERROR_H
-# define FT_INTERNAL_ERROR_H
+#ifndef FT_INTERNAL_ERRORS_H
+# define FT_INTERNAL_ERRORS_H
 
 # include "ft_stdio.h"
 
-typedef struct  s_error
+typedef struct	s_error
 {
-    int     i_errno;
-    char    *s_error;
-}               t_error;
+	int			i_errno;
+	char		*s_error;
+}				t_error;
 
 # define EXE_NAME "minishell"
 # define EXIT_ERR_NON_NUMERIC_ARG 2
@@ -32,15 +32,15 @@ typedef struct  s_error
 # define ERROR_SYNTAX "syntax error near unexpected token"
 
 static const t_error g_internal_errors[INTERNAL_ERRORS_SIZE] = {
-    {ERROR_NOT_VALID_IDENTIFIER, "not a valid identifier"},
-    {ERROR_NUMERIC_ARG_REQUIRED, "numeric argument required"}
+	{ERROR_NOT_VALID_IDENTIFIER, "not a valid identifier"},
+	{ERROR_NUMERIC_ARG_REQUIRED, "numeric argument required"}
 };
 
-enum e_internals_errors
+enum	e_internals_errors
 {
-    ERR_MALLOC_FAILED = -1
+	ERR_MALLOC_FAILED = -1
 };
 
-void    print_internal_error(char *bin, char *arg, int error, int is_str);
+void			print_internal_error(char *bin, char *arg, int error, int str);
 
 #endif
--- srcs/eval/env/get_key.c
+++ srcs/eval/env/get_key.c
@@ -6,24 +6,24 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 16:32:35 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/10 16:35:52 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/16 02:46:22 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strings.h"
 #include "eval.h"
 
-char    *get_key(const char *variable)
+char	*get_key(const char *variable)
 {
-    char *key;
-    char *equal;
+	char	*key;
+	char	*equal;
 
-    equal = ft_strchr(variable, SYM_EQUAL);
-    if (equal == NULL)
-        key = ft_strdup(variable);
-    else
-        key = ft_strndup(variable, equal - variable);
-    if (key == NULL)
-        return (NULL);
-    return (key);
+	equal = ft_strchr(variable, SYM_EQUAL);
+	if (equal == NULL)
+		key = ft_strdup(variable);
+	else
+		key = ft_strndup(variable, equal - variable);
+	if (key == NULL)
+		return (NULL);
+	return (key);
 }
