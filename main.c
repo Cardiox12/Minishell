c1673a4c5be29cca595f6f9bffefc4f2cf176af5
Tony <com.bellavi@tutanota.com>
2020-11-23 03:26:26 +0100

    Norm main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 02:00:27 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/23 00:06:19 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/23 03:26:10 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,46 +27,6 @@ int			g_pid_to_kill = 0;
 int			g_in_eval = 0;
 int			g_quote_parity_error = 0;
 
-char	*get_type(int type)
-{
-	switch (type)
-	{
-		case COMMAND:
-			return ("COMMAND");
-		case STRING:
-			return ("STRING");
-		case OPTION:
-			return ("OPTION");
-		case PIPE:
-			return ("PIPE");
-		case OPERATOR:
-			return ("OPERATOR");
-		case ENV_VARIABLE:
-			return ("ENV_VARIABLE");
-		case REDIRECTION:
-			return ("REDIRECTION");
-		case RAW_STRING:
-			return ("RAW_STRING");
-		case FILE_DESCRIPTOR:
-			return ("FILE DESCRIPTOR");
-		case ARGUMENT:
-			return ("ARGUMENT");
-		default:
-			return ("");
-	}
-}
-
-void	print_queue(t_queue *head)
-{
-	ft_printf("%-30s| %-30s| %-30s\n", "TYPE", "VALUE", "INDEX");
-	ft_printf("----------------------------------------------------------------------\n");
-	while (head != NULL)
-	{
-		ft_printf("%-30s| %-30s| %-30d\n", get_type(head->token.type), head->token.value, head->token.index);
-		head = head->next;
-	}
-}
-
 int		run_shell(void)
 {
 	char	*line;
@@ -80,7 +40,7 @@ int		run_shell(void)
 			return (-1);
 		tokens = lexer(line);
 		if (g_quote_parity_error)
-			ft_putstr_fd("minishell: Error quote is not closed\n", STDERR_FILENO);
+			ft_putstr_fd("minishell: Error quote is not closed\n", 2);
 		else if (parser(line, tokens) == SUCCESS)
 		{
 			g_in_eval = 1;
