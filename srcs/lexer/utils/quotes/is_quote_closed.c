5c7704ba0cb444cc02d0cec781e30ec529a225a4
Tony <com.bellavi@tutanota.com>
2020-11-23 03:14:18 +0100

    Norm quotes

--- srcs/lexer/utils/quotes/is_quote_closed.c
+++ srcs/lexer/utils/quotes/is_quote_closed.c
@@ -6,25 +6,25 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:11:56 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/22 20:34:36 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/23 03:12:04 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-int is_quote_closed(const char *s, char quote)
+int	is_quote_closed(const char *s, char quote)
 {
-    int index;
+	int index;
 
-    index = 0;
-    while (s[index] != '\0')
-    {
-        if (s[index] == quote)
-            return (TRUE);
-        if (is_escaped_by(&s[index], ESC_CHARSET))
-            index++;
-        index++;
-    }
-    g_quote_parity_error = FAILURE;
-    return (FALSE);
+	index = 0;
+	while (s[index] != '\0')
+	{
+		if (s[index] == quote)
+			return (TRUE);
+		if (is_escaped_by(&s[index], ESC_CHARSET))
+			index++;
+		index++;
+	}
+	g_quote_parity_error = FAILURE;
+	return (FALSE);
 }
--- srcs/lexer/utils/quotes/remove_quotes.c
+++ srcs/lexer/utils/quotes/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 18:45:51 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/22 20:39:11 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/23 03:14:03 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,65 @@
 #include "eval.h"
 #include "ft_stdio.h"
 
-static size_t strlen_without_quotes(const char *input, int quote)
+static size_t	strlen_without_quotes(const char *input, int quote)
 {
-    size_t size;
-    size_t index;
+	size_t size;
+	size_t index;
 
-    index = 0;
-    size = 0;
-    while (input[index] != quote && input[index] != '\0')
-    {
-        if (is_escaped_by(&input[index], ESC_BASE_CHARSET))
-        {
-            size++;
-            index++;
-        }
-        else
-            size++;
-        index++;
-    }
-    return (size);
+	index = 0;
+	size = 0;
+	while (input[index] != quote && input[index] != '\0')
+	{
+		if (is_escaped_by(&input[index], ESC_BASE_CHARSET))
+		{
+			size++;
+			index++;
+		}
+		else
+			size++;
+		index++;
+	}
+	return (size);
 }
 
-static void strcpy_without_quotes(char *dst, const char *src, int quote, size_t *idx) 
+static void		strcpy_without_quotes(char *dst, const char *src,
+int quote, size_t *idx)
 {
-    size_t second;
+	size_t second;
 
-    second = 0;
-    while (src[*idx] != quote && src[*idx] != '\0')
-    {
-        if (is_escaped_by(&src[*idx], ESC_BASE_CHARSET))
-        {
-            dst[second++] = src[++(*idx)];
-        }
-        else if (is_escaped_by(&src[*idx], ESC_IMPROVED_CHARSET))
-        {
-            dst[second++] = src[(*idx)++];
-            dst[second++] = src[(*idx)];
-        }
-        else
-        {
-            dst[second++] = src[*idx];
-        }
-        (*idx)++;
-    }
-    if (src[*idx] == quote)
-        (*idx)++;
+	second = 0;
+	while (src[*idx] != quote && src[*idx] != '\0')
+	{
+		if (is_escaped_by(&src[*idx], ESC_BASE_CHARSET))
+		{
+			dst[second++] = src[++(*idx)];
+		}
+		else if (is_escaped_by(&src[*idx], ESC_IMPROVED_CHARSET))
+		{
+			dst[second++] = src[(*idx)++];
+			dst[second++] = src[(*idx)];
+		}
+		else
+		{
+			dst[second++] = src[*idx];
+		}
+		(*idx)++;
+	}
+	if (src[*idx] == quote)
+		(*idx)++;
 }
 
-char    *remove_quotes(const char *input, size_t *index)
+char			*remove_quotes(const char *input, size_t *index)
 {
-    size_t  size;
-    char    *ret;
-    const char    *str = &input[*index];
+	const char		*str = &input[*index];
+	size_t			size;
+	char			*ret;
 
-    size = strlen_without_quotes(&str[1], *str);
-    ret = ft_strnew(size);
-    if (ret == NULL)
-        return (NULL);
-    (*index)++;
-    strcpy_without_quotes(ret, input, *str, index);
-    return (ret);
+	size = strlen_without_quotes(&str[1], *str);
+	ret = ft_strnew(size);
+	if (ret == NULL)
+		return (NULL);
+	(*index)++;
+	strcpy_without_quotes(ret, input, *str, index);
+	return (ret);
 }
