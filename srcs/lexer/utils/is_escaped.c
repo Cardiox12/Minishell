55f6a938690fbc965829805ff8176cc54efec562
Tony <com.bellavi@tutanota.com>
2020-11-23 03:15:13 +0100

    Norm lexer

--- srcs/lexer/utils/is_escaped.c
+++ srcs/lexer/utils/is_escaped.c
@@ -6,16 +6,16 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:12:32 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/22 20:33:47 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/23 03:14:59 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strings.h"
 #include "lexer.h"
 
-int is_escaped_by(const char *s, char *charset)
+int	is_escaped_by(const char *s, char *charset)
 {
-    if (*s == '\0')
-        return (FALSE);
-    return (s[0] == SYM_ESC && ft_strchr(charset, s[1]));
+	if (*s == '\0')
+		return (FALSE);
+	return (s[0] == SYM_ESC && ft_strchr(charset, s[1]));
 }
