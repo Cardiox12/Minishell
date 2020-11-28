cf3df39dd4af1139213cad407e9700cae116285f
Tony <com.bellavi@tutanota.com>
2020-11-28 07:30:41 +0100

    Bugfix on redirection detecion after argument and other tokens

--- srcs/lexer/utils/is_sep.c
+++ srcs/lexer/utils/is_sep.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 21:19:56 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/23 11:45:46 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/28 07:28:42 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	is_sep(int c)
 {
 	return (ft_isspace(c) || c == SYM_PIPE || c == SYM_OPERATOR ||
-	c == SYM_QUOTE || c == SYM_SIMPLE_QUOTE);
+	c == SYM_QUOTE || c == SYM_SIMPLE_QUOTE || is_redirect(c));
 }
 
 int	is_basic_sep(int c)
