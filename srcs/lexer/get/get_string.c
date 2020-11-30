6cfa4b8662aa56b5ae56c16b2f60eee80dd57a40
Tony <com.bellavi@tutanota.com>
2020-11-30 23:37:33 +0100

    Add basic prefixed-string managment

--- srcs/lexer/get/get_string.c
+++ srcs/lexer/get/get_string.c
@@ -6,17 +6,50 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 20:53:39 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/22 20:37:47 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/30 22:31:40 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+#include "expand.h"
+#include "ft_strings.h"
+#include "ft_stdlib.h"
+#include <stdio.h>
+
+char		*strjoin(char **dst, const char *src)
+{
+	size_t size;
+	size_t dstsize;
+
+	if (*dst == NULL)
+		*dst = ft_strdup(src);
+	else
+	{
+		dstsize = ft_strlen(*dst);
+		size = dstsize + ft_strlen(src);
+		*dst = ft_realloc(*dst, sizeof(char) * (size + 1), sizeof(char) * (dstsize + 1));
+		if (*dst == NULL)
+			return (NULL);
+		ft_strlcat(*dst, src, size + 1);
+		(*dst)[size] = '\0';
+	}
+	return (*dst);
+}
+
+static char	*escape(char **content)
+{
+	str_replace(content, "$", "\\$");
+	str_replace(content, "\\", "\\\\");
+
+	return (*content);
+}
 
 int	get_string(t_queue **head, const char *input, size_t index)
 {
 	size_t	previous;
 	int		type;
-	char	*content;
+	char	*content = NULL;
+	char	*next;
 
 	previous = index;
 	if (input[index] == SYM_SIMPLE_QUOTE)
@@ -30,6 +63,15 @@ int	get_string(t_queue **head, const char *input, size_t index)
 	if (!is_quote_closed(&input[index + 1], input[index]))
 		return (index);
 	content = remove_quotes(input, &index);
+	while (ft_isquote(input[index]))
+	{
+		int escape_mode = input[index] == SYM_SIMPLE_QUOTE;
+		next = remove_quotes(input, &index);
+		if (escape_mode)
+			escape(&next);
+		strjoin(&content, next);
+		free(next);
+	}
 	enqueue(head, (t_token){
 		.type = type,
 		.value = content,
