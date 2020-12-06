e8123e9aa98581e40b11739a1de78ff7af1199bd
Tony <com.bellavi@tutanota.com>
2020-12-07 00:44:22 +0100

    Norm environ.c

--- environ.c
+++ environ.c
@@ -6,16 +6,18 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 07:11:01 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/30 07:27:43 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/12/07 00:43:41 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environ.h"
 
+typedef char*	t_string;
+
 void	export_basic_environ(void)
 {
 	char	cwd[PATH_MAX + 1];
-	size_t index;
+	size_t	index;
 
 	index = 0;
 	while (index < BASIC_ENVIRON_SIZE)
