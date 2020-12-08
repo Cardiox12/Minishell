2b195c870ffccffcabffeeede35220912422c61b
Tony <com.bellavi@tutanota.com>
2020-12-08 21:41:05 +0100

    Manage operator and pipe escape and fix bug

--- srcs/eval/variables/expand.c
+++ srcs/eval/variables/expand.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 01:10:43 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/12/08 20:59:25 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/12/08 21:40:11 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static char	*tilde_expand(char **src)
 	return (*src);
 }
 
-char		*basic_expand(char **src)
+char		*basic_expand(char **src, char *charset)
 {
 	char	*new;
 	t_slice	slice;
@@ -39,9 +39,11 @@ char		*basic_expand(char **src)
 	new = *src;
 	while (new[slice.begin] != '\0')
 	{
-		if (is_escaped_by(&new[slice.begin], ESC_IMPROVED_CHARSET))
+		if (is_escaped_by(&new[slice.begin], charset))
 		{
 			removal(&new, slice);
+			if (new[slice.begin] != '\0')
+				slice.begin++;
 		}
 		else if (is_bash_var(new, slice))
 		{
@@ -64,8 +66,14 @@ char		*expand(const char *src, int tok_type)
 	if ((new = ft_strdup(src)) == NULL)
 		return (NULL);
 	if (tok_type == ARGUMENT)
+	{
 		tilde_expand(&new);
-	basic_expand(&new);
+		basic_expand(&new, ESC_CHARSET);
+	}
+	else
+	{
+		basic_expand(&new, ESC_IMPROVED_CHARSET);
+	}
 	return (new);
 }
 
