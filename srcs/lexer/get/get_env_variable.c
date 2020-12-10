178b3d5445c58f03415b61e0c7294d7990b34fef
Tony <com.bellavi@tutanota.com>
2020-12-10 10:08:38 +0100

    Fix bug on variables

--- srcs/lexer/get/get_env_variable.c
+++ srcs/lexer/get/get_env_variable.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 20:54:16 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/12/09 00:22:47 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/12/10 10:03:38 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,23 @@ int	get_env_variable(t_queue **head, const char *input, size_t index)
 	const size_t	previous = index;
 	char			*value;
 
-	while (input[index] != '\0' && !is_sep(input[index]))
-		index++;
-	value = ft_strndup(&input[previous], index - previous);
+	if (input[index + 1] == '?' || ft_isdigit(input[index + 1]))
+	{
+		index += 2;
+		value = ft_strndup(&input[previous], index - previous);
+	}
+	else
+	{
+		while (input[index] != '\0' && !is_sep(input[index]))
+			index++;
+		value = ft_strndup(&input[previous], index - previous);
+	}
 	value = str_expand(&value, ENV_VARIABLE);
 	enqueue(head, (t_token){
 		.type = ENV_VARIABLE,
 		.value = value,
 		.index = previous,
-		.prefixed = FALSE
+		.prefixed = !is_sep(input[index])
 	});
 	return (index);
 }
