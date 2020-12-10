8b8e4dd31adf0d63f0140736fbf1c5f4c285e979
Tony <com.bellavi@tutanota.com>
2020-12-10 01:13:43 +0100

    Fix bug on redirection syntax error

--- srcs/parser/parser/commands.c
+++ srcs/parser/parser/commands.c
@@ -6,31 +6,43 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 02:04:26 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/12/10 00:09:40 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/12/10 01:04:41 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+static int	check_redirection(t_interpret *inter)
+{
+	int out;
+
+	if (inter->current == NULL)
+		return (_EOF_);
+	if (inter->current->token.type == REDIRECTION)
+	{
+		out = eat(inter, (int[1]){REDIRECTION}, 1);
+		if (out == _EOF_)
+			return (SYNTAX_ERROR_NEWLINE);
+	}
+	return (SUCCESS);
+}
+
 int	commands(t_interpret *inter)
 {
 	int out;
 
+	out = check_redirection(inter);
+	if (out != 0)
+		return (out);
 	out = eat(inter, g_non_terminal_tokens, ARR_NON_TERM_SIZE);
 	if (out != 0)
 		return (out);
 	while (is_type(inter->current->token.type, g_non_terminal_tokens,
 	ARR_NON_TERM_SIZE))
 	{
-		if (inter->current->token.type == REDIRECTION)
-		{
-			out = eat(inter, (int[1]){REDIRECTION}, 1);
-			if (out == _EOF_)
-				return (SYNTAX_ERROR_NEWLINE);
-			out = eat(inter, (int[1]){FILE_DESCRIPTOR}, 1);
-			if (out != 0)
-				return (out);
-		}
+		out = check_redirection(inter);
+		if (out != 0)
+			return (out);
 		out = eat(inter, g_non_terminal_tokens, ARR_NON_TERM_SIZE);
 		if (out != 0)
 			return (out);
